5f30f1c077723c0e38b20a25d412a1baa21befbb
nanshanvv <shuchangwen0922@gmail.com>
2025-05-08 18:37:20 -0400

    v1.0

--- sc-dev-tech-trigger/src/main/java/cn/shuchang/sc/dev/tech/trigger/http/OllamaController.java
+++ sc-dev-tech-trigger/src/main/java/cn/shuchang/sc/dev/tech/trigger/http/OllamaController.java
@@ -37,7 +37,7 @@ public class OllamaController implements IAiService {
      */
     @RequestMapping(value = "generate", method = RequestMethod.GET)
     @Override
-    public ChatResponse generate(@RequestParam String model, @RequestParam String message) {
+    public ChatResponse generate(@RequestParam("model") String model, @RequestParam("message") String message) {
         return chatClient.call(new Prompt(message, OllamaOptions.create().withModel(model)));
     }
     /**
@@ -45,7 +45,7 @@ public class OllamaController implements IAiService {
      */
     @RequestMapping(value = "generate_stream", method = RequestMethod.GET)
     @Override
-    public Flux<ChatResponse> generateStream(@RequestParam String model, @RequestParam String message) {
+    public Flux<ChatResponse> generateStream(@RequestParam("model") String model, @RequestParam("message") String message) {
         return chatClient.stream(new Prompt(message, OllamaOptions.create().withModel(model)));
     }
 
